784b9bc54de979c36e3d997d7f949cee6f3f1233
Brianna Jordan <bryvjordan9@gmail.com>
2019-07-21 01:38:59 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,12 @@ puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_with_language (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
+introduction_with_language ("Dan", "Ember.js")
 
 def introduction_with_language_optional (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+introduction_with_languag_optional ("Edwin", "Ruby")
+  
